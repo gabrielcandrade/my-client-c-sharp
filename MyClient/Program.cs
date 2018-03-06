92d5848e5c0bfa938ccc7a4beefaa210f865d0de
Gabriel Andrade <gabrielcandrade01@gmail.com>
2018-03-06 14:08:31 -0300

    Subindo projeto.

---
+++ MyClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyCodeLibraryComplete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebpage("http://www.google.com.br");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
